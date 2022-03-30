7d4e7f4b7467b74e98f1cd1663f758bffdbf0dcb
pullathomas <brian.m.odonnell@gmail.com>
2022-03-30 11:39:50 -0500

    added some parameters

--- deploy-edge-vm.sh
+++ deploy-edge-vm.sh
@@ -13,7 +13,9 @@ Help()
    echo "-ar      Azure Region (optional / default EUS2)"    
    echo "-rg      Resource Group Name (optional)"
    echo "-vmname  Name of new VM (optional)"
-   echo "-h      Show Help"
+   echo "-vnet    Existing VNET to deploy into (optional)"
+   echo "-subnet  Existing subnet in VNET (optional)"
+   echo "-h       Show Help"
    echo
    exit;
 }
@@ -23,42 +25,61 @@ if [ -z "$1" ]; then Help exit; fi
 while [ ! -z "$1" ];
     do
         case "$1" in
-        -sid) shift;SCOPEID=$1;;
-        -did) shift;DEVICEID=$1;;
-        -key) shift;SASKEY=$1;;
-        -ar) shift;REGION=$1;;
-        -rg) shift;RESOURCEGROUP=$1;;
-        -vmname) shift;VMNAME=$1;;
+        -sid) shift;SID=$1;;
+        -did) shift;DID=$1;;
+        -key) shift;KEY=$1;;
+        -ar) shift;AZRG=$1;;
+        -rg) shift;RGRP=$1;;
+        -vmname) shift;VM=$1;;
+        -vnet) shift;VN=$1;;
+        -subnet) shift;SUB=$1;;
         -h) Help exit;;
     esac
     shift
 done
 
-if [ ! -z "$SCOPEID" ]; then Help exit; fi
-if [ ! -z "$DEVICEID" ]; then Help exit; fi
-if [ ! -z "$SASKEY" ]; then Help exit; fi
+SCOPEID=${SID}
+DEVICEID=${DID}
+SASKEY=${KEY}
+REGION=${AZRG}
+RESOURCEGROUP=${RGRP}
+VMNAME=${VM}
+VNET=${VN}
+SUBNET=${SUB}
 
-if [ ! -z "$REGION" ]; then 
-    $REGION="eus2"
+if [ -z "$SCOPEID" ]; then Help exit; fi
+if [ -z "$DEVICEID" ]; then Help exit; fi
+if [ -z "$SASKEY" ]; then Help exit; fi
+
+if [ -z "$REGION" ]; then 
+    REGION="eus2"
 fi
 
-if [ ! -z "$RESOURCEGROUP" ]; then 
-    $RESOURCEGROUP="rg-$(echo $random | md5sum | head -c 10; echo;)"
+if [ -z "$RESOURCEGROUP" ]; then 
+    RESOURCEGROUP="rg-$(echo $random | md5sum | head -c 10; echo;)"
 fi
 
-if [ ! -z "$VMNAME" ]; then 
-    $VMNAME="vm-$(echo $random | md5sum | head -c 10; echo;)"
+if [ -z "$VMNAME" ]; then 
+    VMNAME="vm-$(echo $random | md5sum | head -c 10; echo;)"
 fi
+
 
 #create resource group
 if [ $(az group exists --name $RESOURCEGROUP) = false ]; then
     az group create --name $RESOURCEGROUP --location $REGION
 fi
 
-az vm create --resource-group $RESOURCEGROUP --name $VMNAME --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest --size Standard_DS2_v3  --admin-username azureuser --generate-ssh-keys
+echo $RESOURCEGROUP
+if [ -z "$VNET" ]; then
+    echo "az vm create --resource-group $RESOURCEGROUP --name $VMNAME --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest --size Standard_DS2_v2  --admin-username azureuser --generate-ssh-keys"
+    az vm create --resource-group $RESOURCEGROUP --name $VMNAME --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest --size Standard_DS2_v2  --admin-username azureuser --generate-ssh-keys
+else
+    echo "az vm create --resource-group $RESOURCEGROUP --name $VMNAME --vnet-name $VNET --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest --size Standard_DS2_v2  --admin-username azureuser --generate-ssh-keys"
+    az vm create --resource-group $RESOURCEGROUP --name $VMNAME --vnet-name $VNET --subnet $SUBNET --image Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:latest --size Standard_DS2_v2  --admin-username azureuser --generate-ssh-keys
+fi
 az vm extension set \
-  --resource-group exttest \
-  --vm-name exttest \
+  --resource-group $RESOURCEGROUP \
+  --vm-name $VMNAME \
   --name customScript \
   --publisher Microsoft.Azure.Extensions \
   --protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/ms-us-rcg-app-innovation/iot-central-edge-deploy/main/iot-edge-config-bash.sh\"],\"commandToExecute\": \"sudo bash iot-edge-config-bash.sh $SCOPEID $DEVICEID $SASKEY\"}"
