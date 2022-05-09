db00668f91ad55e8fcdcc6b1e22bfc6eeebcd24e
pullathomas <brian.m.odonnell@gmail.com>
2022-05-09 16:29:11 -0500

    fixed yaml write

--- iot-edge-config-bash.sh
+++ iot-edge-config-bash.sh
@@ -42,51 +42,41 @@ echo 'hostname = "'`hostname`'"' > $FILE_NAME
 echo '' >> $FILE_NAME
 echo '## DPS provisioning with symmetric key' >> $FILE_NAME
 echo 'provisioning:' >> $FILE_NAME
-echo 'source = "dps"' >> $FILE_NAME
+echo '  source: "dps"' >> $FILE_NAME
 echo '' >> $FILE_NAME
-echo 'global_endpoint = "https://global.azure-devices-provisioning.net"' >> $FILE_NAME
-echo 'id_scope = "'$SCOPE_ID'"' >> $FILE_NAME
+echo '  global_endpoint: "https://global.azure-devices-provisioning.net"' >> $FILE_NAME
+echo '  scope_id: "'$SCOPE_ID'"' >> $FILE_NAME
 echo '' >> $FILE_NAME
-echo 'provisioning.attestation:' >> $FILE_NAME
-echo 'method = "symmetric_key"' >> $FILE_NAME
-echo 'registration_id = "'$REGISTRATION_ID'"' >> $FILE_NAME
+echo '  attestation:' >> $FILE_NAME
+echo '    method: "symmetric_key"' >> $FILE_NAME
+echo '    registration_id: "'$REGISTRATION_ID'"' >> $FILE_NAME
 echo '' >> $FILE_NAME
-echo 'symmetric_key = { value = "'$SYMMETRIC_KEY'" }' >> $FILE_NAME
+echo '    symmetric_key: "'$SYMMETRIC_KEY'"' >> $FILE_NAME
 echo '' >> $FILE_NAME
-echo 'aziot_keys:' >> $FILE_NAME
-echo '' >> $FILE_NAME
-echo 'preloaded_keys:' >> $FILE_NAME
-echo '' >> $FILE_NAME
-echo 'cert_issuance:' >> $FILE_NAME
+echo '  always_reprovision_on_startup: true' >> $FILE_NAME
+echo '  dynamic_reprovisioning: false' >> $FILE_NAME
 echo '' >> $FILE_NAME
 echo 'agent:' >> $FILE_NAME
-echo 'name = "edgeAgent"' >> $FILE_NAME
-echo 'type = "docker"' >> $FILE_NAME
-echo 'imagePullPolicy = "on-create"' >> $FILE_NAME
+echo '  name: "edgeAgent"' >> $FILE_NAME
+echo '  type: "docker"' >> $FILE_NAME
+echo '  env: {}' >> $FILE_NAME
+echo '  config:' >> $FILE_NAME
+echo '    image: "mcr.microsoft.com/azureiotedge-agent:1.1"' >> $FILE_NAME
+echo '    auth: {}' >> $FILE_NAME
 echo '' >> $FILE_NAME
-echo 'agent.config:' >> $FILE_NAME
-echo 'image = "mcr.microsoft.com/azureiotedge-agent:1.1"' >> $FILE_NAME
-echo '' >> $FILE_NAME
-echo 'agent.config.createOptions:' >> $FILE_NAME
-echo '' >> $FILE_NAME
-echo 'agent.env:' >> $FILE_NAME
-echo '' >> $FILE_NAME
+echo 'hostname: "'$REGISTRATION_ID'"' >> $FILE_NAME
 echo 'connect:' >> $FILE_NAME
-echo 'workload_uri = "unix:///var/run/iotedge/workload.sock"' >> $FILE_NAME
-echo 'management_uri = "unix:///var/run/iotedge/mgmt.sock"' >> $FILE_NAME
+echo '  workload_uri: "unix:///var/run/iotedge/workload.sock"' >> $FILE_NAME
+echo '  management_uri: "unix:///var/run/iotedge/mgmt.sock"' >> $FILE_NAME
 echo '' >> $FILE_NAME
 echo 'listen:' >> $FILE_NAME
-echo 'workload_uri = "fd://iotedge.socket"' >> $FILE_NAME
-echo 'management_uri = "fd://iotedge.mgmt.socket"' >> $FILE_NAME
-echo 'min_tls_version = "tls1.0"' >> $FILE_NAME
-echo '' >> $FILE_NAME
-echo 'watchdog:' >> $FILE_NAME
-echo 'max_retries = "infinite"' >> $FILE_NAME
+echo '  workload_uri: "fd://iotedge.socket"' >> $FILE_NAME
+echo '  management_uri: "fd://iotedge.mgmt.socket"' >> $FILE_NAME
 echo '' >> $FILE_NAME
 echo 'homedir: "/var/lib/iotedge"' >> $FILE_NAME
 echo '' >> $FILE_NAME
 echo 'moby_runtime:' >> $FILE_NAME
-echo 'uri = "unix:///var/run/docker.sock"' >> $FILE_NAME
-echo 'network = "azure-iot-edge"' >> $FILE_NAME
+echo '  uri: "unix:///var/run/docker.sock"' >> $FILE_NAME
+echo '  network: "azure-iot-edge"' >> $FILE_NAME
 echo "Apply settings - this will restart the edge"
 sudo systemctl restart iotedge
